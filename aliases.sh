e4e76a014ea3c072502e8479c3152cc18d92eee5
snakeonex <romakcz@gmail.com>
2023-12-27 21:13:16 +0100

    cleaup + new ohmyzsh theme

--- aliases.sh
+++ aliases.sh
@@ -1,23 +1,5 @@
 alias o=xdg-open
 alias vim=nvim
 alias vi=nvim
-alias tensor='source activate tensorflow'
-alias deactivate='source deactivate'
-alias torch='source activate pytorch'
-alias fastai='source activate fastai'
-alias mongod='sudo mongod --dbpath=/var/lib/mongodb'
-alias redshift='redshift -l 49.3088:14.147'
-alias tusk='/opt/Tusk/tusk-app'
 
 alias c='xclip -selection clipboard'
-
-# uni
-alias opt='cd ~/ownCloud/semester_4/opt'
-alias gvg='cd ~/ownCloud/semester_4/gvg'
-alias dbs='cd ~/ownCloud/semester_4/dbs'
-alias pdv='cd ~/ownCloud/semester_4/pdv'
-alias zui='cd ~/ownCloud/semester_4/zui'
-alias fup='cd ~/ownCloud/semester_4/fup'
-
-# added by Anaconda3 installer
-export PATH="/home/snakeone/anaconda3/bin:$PATH"
