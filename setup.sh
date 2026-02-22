28c09e974460c96b544a866178cf8f94a52ef13d
snakeonex <romakcz@gmail.com>
2026-02-22 23:14:34 +0100

    Add ghostty and tmux symlinks to setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,12 @@ ln -s ~/dotfiles/init.lua ~/.config/nvim/init.lua
 
 mv ~/.config/fish/config.fish ~/.config/fish/config.fish_backup
 ln -s ~/dotfiles/config.fish ~/.config/fish/config.fish
+
+mkdir -p ~/.config/ghostty
+mv ~/.config/ghostty/config ~/.config/ghostty/config_backup 2>/dev/null
+mv ~/.config/ghostty/tab-style.css ~/.config/ghostty/tab-style.css_backup 2>/dev/null
+ln -s ~/dotfiles/ghostty_config ~/.config/ghostty/config
+ln -s ~/dotfiles/ghostty_tab-style.css ~/.config/ghostty/tab-style.css
+
+mv ~/.tmux.conf ~/.tmux.conf_backup 2>/dev/null
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
